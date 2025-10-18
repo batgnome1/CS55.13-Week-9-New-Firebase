5f388062787632546bbde3edc62e2de99227872e
Andrew Mayfield <batgnome@gmail.com>
2025-10-17 19:53:48 -0700

    more errors fixed

--- src/components/ModuleListings.jsx
+++ src/components/ModuleListings.jsx
@@ -77,7 +77,7 @@ export default function ModuleListings({
   }, [router, filters]);
 
   useEffect(() => {
-    return getModuleSnapshotbyId((data) => {
+    return getModuleSnapshot((data) => {
       setModules(data);
     }, filters);
   }, [filters]);
