0520059ca12d1038013eec1cc74d1989f71c6729
Andrew Mayfield <batgnome@gmail.com>
2025-10-26 12:35:42 -0700

    writing up some data

--- src/lib/randomData.js
+++ src/lib/randomData.js
@@ -91,142 +91,142 @@ export const randomData = {
       genre: "Action",
       descriptions: 
         [
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
-          "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
-          "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur"
+          "You're surrounded by mercenaries out for blood. Good thing you've got more bullets than brain cells. Time to shoot your way out of trouble.",
+          "A genocidal alien has crash landed on Earth. Your muscles are the only thing between it and total planetary destruction.",
+          "A little girl has been kidnapped. The coppers are incompetent, the mayor corrupt, and the family is desperate. You're a washed up has-been bodyguard but it's time to dry out and start punching bad guys."
         ]
     },
   {
       genre: "Adventure",
       descriptions: 
         [
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
-          "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
-          "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur"
+          "You're a globetrotter in search of the last undiscovered ancient civilization. Traverse the jungles to find the mythical city of Xolum-ra.",
+          "Hired by reclusive billionaire Rex Menace, your team is tasked with finding the lost treasure of Mahn Dien. But all is not what it seems...",
+          "Pirates have pillaged your hometown and stolen the beloved golden crow statuette from the museum. You have to hunt them down and retrieve that bird!"
         ]
   },
     {
       genre: "Comedy",
       descriptions: 
         [
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
-          "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
-          "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur"
+          "	Your set at the Giggle Factory has gone viral and now you've been hired as a writer on the Late Night Show. Are you actually funny or was your viral set a flash in the pan? ",
+          "You're a comedy troupe traveling across the country performing for the masses.",
+          "Improv, one liners, slapstick - joke for your life in this module straight out of the funny pages!"
         ]
   },
     {
       genre: "Fantasy",
       descriptions: 
         [
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
-          "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
-          "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur"
+          "The dragons have returned and are causing problems in the township of Crescent Grove. You're on a quest to find the sorcerer responsible and compel them to banish the dragons back to the nether realm.",
+          "The high council of Er has forbid magic across the realm. Far in the north a cohort of casters band together to rebel against the law.",
+          "The gnomes have built a flying machine and intend to fly to the moon to mine precious metals. You must decide to join them or stop the operation for good."
         ]
   },
     {
       genre: "Noir",
       descriptions: 
         [
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
-          "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
-          "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur"
+          "A woman, a gun, a car, and a million bucks. What could go wrong? Navigate the booze fueled twists and turns of this murder mystery.",
+          "That damn dame just spent your last dime. The bookie's calling and the rent's due. But the hot corruption case that just landed in your lap could turn your luck...",
+          "You've got one bullet, one cigarette, one beer and a hell of a lot of problems. "
         ]
   },
     {
       genre: "Horror",
       descriptions: 
         [
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
-          "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
-          "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur"
+          "Spooky vibes, thrilling rides and haunted joints. Never has being scared to death been so fun!",
+          "Shriek into the night as you become the hunted. A vampire on the loose wreaks havoc in the village, your party arrives to hunt the undead menace and save the village. But everything is not what it seems...",
+          "Stare into the cosmic abyss, question reality and grasp the frayed ends of sanity!"
         ]
   },
     {
       genre: "Romance",
       descriptions: 
         [
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
-          "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
-          "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur"
+          "Dance with someone and feel the heat. This module has it all - lust, betrayal, love, miscommunication, and threesomes. Don't miss the chance to roleplay your deepest desires.",
+          "Ride into bliss with this fiery module chock full of thrusting and throbbing, sucking and cucking, kissing and hugging.",
+          "Meet cute in the gothic castle and turn into a creature of the night. "
         ]
   },
     {
       genre: "Scifi",
       descriptions: 
         [
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
-          "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
-          "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur"
+          "In a parallel reality where their characters don't exist, players must discover why they've been erased from history and who's responsible!",
+          "When the sun dies, residents of Planet O migrate to space station Ugh. Natives and migrants clash as resources become scarce. Players are caught in the middle of the scuffle and must find a path to peace.",
+          "AI scumlords have written a lucrative algorithm that could solve the world's problems but melts the minds of the destitute in the process. Players must decide if that's an acceptable cost for the promise of a utopian future."
         ]
   },
     {
       genre: "Western",
       descriptions: 
         [
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
-          "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
-          "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur"
+          "Armed only with a sixgun, faded wanted poster, and this morning's hangover players must muster all they got to outwit and outdo the dreaded criminal Big Earl in this module.",
+          "Tumbelweed and twisters. Whiskey and wagons. Prospectors and prostitutes. Players navigate it all in this wild west module!",
+          "A robbery gone awry.  A train derailment. The U.S. Marshals and a zealot out for revenge. This module packs a punch!"
         ]
   },
 ],
 
   moduleReviews: [
-    { text: "The food was exceptional, absolutely loved it!", rating: 5 },
-    { text: "Delicious dishes and excellent service!", rating: 5 },
-    { text: "The flavors were so rich and satisfying.", rating: 5 },
-    { text: "Great ambiance and friendly staff.", rating: 5 },
-    { text: "A delightful culinary experience!", rating: 5 },
+    { text: "The drama was exceptional, absolutely loved it!", rating: 5 },
+    { text: "My favorite Gnomercy module to date!", rating: 5 },
+    { text: "The puzzles were so intricate, solving them was super satisfying.", rating: 5 },
+    { text: "Great story and thrilling adventure.", rating: 5 },
+    { text: "A delightful roleplaying experience!", rating: 5 },
     {
-      text: "Mouthwatering dishes that left me wanting more.",
+      text: "Thrilling adventure that left me wanting more.",
       rating: 5,
     },
-    { text: "Perfectly cooked and seasoned meals.", rating: 5 },
-    { text: "Incredible presentation and top-notch taste.", rating: 5 },
-    { text: "Good food and cozy atmosphere.", rating: 4 },
-    { text: "Enjoyed the meal, would come back again.", rating: 4 },
-    { text: "Tasty options and reasonable prices.", rating: 4 },
-    { text: "Friendly staff, but the food was just okay.", rating: 3 },
+    { text: "Perfectly structured and designed module.", rating: 5 },
+    { text: "Incredible presentation and top-notch action.", rating: 5 },
+    { text: "Good vibes and cozy atmosphere.", rating: 4 },
+    { text: "Enjoyed the adventure, would definitely recommend for new players", rating: 4 },
+    { text: "Reasonable encounters and well paced.", rating: 4 },
+    { text: "Really cool premise, but the adventure was just okay.", rating: 3 },
     { text: "Decent experience, but nothing extraordinary.", rating: 3 },
-    { text: "The food was average, could be better.", rating: 3 },
+    { text: "Average, could be better.", rating: 3 },
     {
-      text: "Service was slow, and the dishes were disappointing.",
+      text: "Story was poorly paced, and the puzzles were disappointing.",
       rating: 2,
     },
     { text: "Expected more, but left unsatisfied.", rating: 2 },
-    { text: "Underwhelming taste and presentation.", rating: 2 },
+    { text: "Underwhelming NPCs and presentation.", rating: 2 },
     { text: "Disappointing experience overall.", rating: 2 },
     {
-      text: "The food was terrible, will never go back again.",
+      text: "This module was terrible, too difficult and confusing",
       rating: 1,
     },
     {
-      text: "Worst restaurant experience I've had so far.",
+      text: "Worst Gnomercy experience I've had so far.",
       rating: 1,
     },
-    { text: "Avoid this place, the food was inedible.", rating: 1 },
-    { text: "Unpleasant service and tasteless dishes.", rating: 1 },
-    { text: "Simply outstanding! A culinary masterpiece.", rating: 5 },
-    { text: "Couldn't get enough of the amazing flavors.", rating: 5 },
+    { text: "Avoid this module, the story was inane.", rating: 1 },
+    { text: "Unpleasant and tasteless story.", rating: 1 },
+    { text: "Simply outstanding! An absolute masterpiece.", rating: 5 },
+    { text: "Couldn't get enough of the amazing action.", rating: 5 },
     { text: "Top-notch quality, worth every penny.", rating: 5 },
-    { text: "Highly recommended for food enthusiasts.", rating: 5 },
-    { text: "Exquisite dishes that pleased my taste buds.", rating: 5 },
-    { text: "A gem of a place, impeccable in every aspect.", rating: 5 },
-    { text: "Excellent selection of dishes on the menu.", rating: 5 },
-    { text: "A fantastic dining experience overall.", rating: 5 },
+    { text: "Highly recommended for roleplaying enthusiasts.", rating: 5 },
+    { text: "Exquisite puzzles that scratched that itch.", rating: 5 },
+    { text: "A gem of a module, impeccable in every aspect.", rating: 5 },
+    { text: "Excellent selection of encounters in this module.", rating: 5 },
+    { text: "A fantastic roleplaying experience overall.", rating: 5 },
     {
       text: "The atmosphere was lovely, perfect for a date night.",
       rating: 4,
     },
-    { text: "Good food, but the service could be improved.", rating: 4 },
+    { text: "Good premise, but the encounters could have been better.", rating: 4 },
     {
-      text: "Pleasantly surprised by the variety of flavors.",
+      text: "Pleasantly surprised by the variety of loot.",
       rating: 4,
     },
-    { text: "Well-prepared dishes and friendly staff.", rating: 4 },
-    { text: "Satisfying meals, suitable for a quick bite.", rating: 4 },
-    { text: "The food was okay, nothing special.", rating: 3 },
+    { text: "Well-designed puzzles and engaging encounters.", rating: 4 },
+    { text: "Satisfying puzzles, suitable for any player regardless of experience.", rating: 4 },
+    { text: "The module was okay, nothing special.", rating: 3 },
     {
-      text: "Service could be better, but the taste was fine.",
+      text: "module could be better, but the experience was fine.",
       rating: 3,
     },
     {
@@ -234,9 +234,9 @@ export const randomData = {
       rating: 3,
     },
     { text: "Expected more value for the price.", rating: 2 },
-    { text: "Mediocre taste and lackluster presentation.", rating: 2 },
+    { text: "Mediocre story and lackluster presentation.", rating: 2 },
     {
-      text: "Regret spending money on such a disappointing meal.",
+      text: "Regret spending money on such a disappointing module.",
       rating: 2,
     },
     { text: "Not up to par with my expectations.", rating: 2 },
