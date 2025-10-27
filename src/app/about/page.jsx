ba1f6db348a9ef5e0a07c12d95d2787835f2eedc
Andrew Mayfield <batgnome@gmail.com>
2025-10-27 09:26:34 -0700

    more about page stuff

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -5,10 +5,11 @@ export default function AboutPage() {
     <main>
       <h1 className="about"> GNOMERCY</h1>
       <h2 className="about">A Tabletop Role-playing Game System</h2>
-      <article>
+      <article className="aboutarticle">
         <p>Gnomercy is a generic TTRPG system - a foundation of rules and mechanics without setting, 
             genre or lore. It is a highly customizable, flexible system designed to accommodate modding / homebrewing. 
         </p>
+        <br></br>
         <p>
             The lore, worldbuilding, genre and story for Gnomercy are provided via Modules - self contained campaigns
             tailored for a set number of players to roleplay specific scenarios. 
